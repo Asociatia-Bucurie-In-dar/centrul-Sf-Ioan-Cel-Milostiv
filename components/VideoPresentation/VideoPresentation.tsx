8ed7193a95324c7e01841f6295c70ee329a09772
Fabian Ghenof <fabianghenof@techie.com>
2026-08-04 14:50:54 +0300

    new video added

--- components/VideoPresentation/VideoPresentation.tsx
+++ components/VideoPresentation/VideoPresentation.tsx
@@ -3,7 +3,7 @@ import {TitleWithDescription} from "@/components/Common/TitleWithDescription";
 import {useTranslations} from "next-intl";
 import classes from './VideoPresentation.module.css';
 
-const YOUTUBE_VIDEO_ID = 'YOUR_VIDEO_ID';
+const VIDEO_IDS = ['lL30UGT8NxQ', 'ZDVKAJHxNFc'];
 
 export function VideoPresentation() {
     const t = useTranslations('VIDEO_PRESENTATION');
@@ -11,17 +11,22 @@ export function VideoPresentation() {
     return (
         <Container size="lg" className={classes.wrapper}>
             {/* <TitleWithDescription title={t('TITLE')} description={t('DESCRIPTION')}/> */}
-            <div className={classes.videoContainer}>
-                <iframe
-                    className={classes.iframe}
-                    src={`https://www.youtube-nocookie.com/embed/lL30UGT8NxQ`}
-                    title={t('TITLE')}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerPolicy="strict-origin-when-cross-origin"
-                    allowFullScreen
-                    loading="lazy"
-                />
+            <div className={classes.videos}>
+                {VIDEO_IDS.map((videoId, index) => (
+                    <div key={index} className={classes.videoContainer}>
+                        <iframe
+                            className={classes.iframe}
+                            src={`https://www.youtube-nocookie.com/embed/${videoId}`}
+                            title={t('TITLE')}
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerPolicy="strict-origin-when-cross-origin"
+                            allowFullScreen
+                            loading="lazy"
+                        />
+                    </div>
+                ))}
             </div>
         </Container>
     );
 }
+
